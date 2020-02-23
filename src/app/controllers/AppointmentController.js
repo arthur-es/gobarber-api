b9e06f580ea02ae108d2e932c8c6ad6566d759ca
arthur-es <arthurcastroes@gmail.com>
2020-02-23 18:27:37 -0300

    appointment controller

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,45 @@
+import * as Yup from "yup";
+import Appointment from "../models/Appointment";
+import User from "../models/User";
+
+class AppointmentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required()
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        error: "Yup validation failed when trying to create an appointment"
+      });
+    }
+
+    const { provider_id, date } = req.body;
+
+    try {
+      // Check if provider_id is a provider
+      const isProvider = await User.findOne({
+        where: {
+          id: provider_id,
+          provider: true
+        }
+      });
+      if (!isProvider) {
+        return res.status(401).json({ error: "Provide a valid provider_id" });
+      }
+
+      const appointment = await Appointment.create({
+        user_id: req.userId,
+        provider_id,
+        date
+      });
+
+      return res.json(appointment);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+export default new AppointmentController();
