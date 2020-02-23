c286214a025e7e2155434d9f33ea04ae10bcf5aa
arthur-es <arthurcastroes@gmail.com>
2020-02-23 20:33:26 -0300

    update notification to read

--- src/routes.js
+++ src/routes.js
@@ -25,6 +25,7 @@ routes.post("/files", upload.single("file"), FileController.store);
 routes.get("/appointments", AppointmentController.index);
 routes.post("/appointments", AppointmentController.store);
 routes.get("/notifications", NotificationController.index);
+routes.put("/notifications/:id", NotificationController.update);
 
 routes.get("/schedule", ScheduleController.index);
 routes.put("/users", UserController.update);
