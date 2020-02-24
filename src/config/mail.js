323243789c67f087777ef530d65cf84410746996
arthur-es <arthurcastroes@gmail.com>
2020-02-23 21:56:48 -0300

    sending email with mailtrap to provider when someone cancels a appointment

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "bc8501dee201c1",
+    pass: "85fd624fd88370"
+  },
+  secure: false,
+  default: {
+    from: "Equipe GoBarber <noreply@gobarber.com>"
+  }
+};
