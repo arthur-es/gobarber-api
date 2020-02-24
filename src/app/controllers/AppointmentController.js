cb25885ab54e2ee76d236aee525c36cb3276c32c
arthur-es <arthurcastroes@gmail.com>
2020-02-23 21:39:50 -0300

    cancel appointment with at least 2 hours prior

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,5 +1,5 @@
 import * as Yup from "yup";
-import { isBefore, startOfHour, parseISO, format } from "date-fns";
+import { isBefore, startOfHour, parseISO, format, subHours } from "date-fns";
 import pt from "date-fns/locale/pt";
 
 import Appointment from "../models/Appointment";
@@ -113,6 +113,33 @@ class AppointmentController {
         content: `Novo agendamento de ${user.name} para ${formattedDate}`,
         user: provider_id
       });
+
+      return res.json(appointment);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const appointment = await Appointment.findByPk(req.params.id);
+      if (appointment.user_id !== req.userId) {
+        return res.status(400).json({
+          error: "You don't have permission to cancel this appointment."
+        });
+      }
+
+      const dateWithSub = subHours(appointment.date, 2);
+
+      if (isBefore(dateWithSub, new Date())) {
+        return res.status(401).json({
+          error:
+            "Can't cancel without at least 2 hours prior to the appointment time"
+        });
+      }
+
+      appointment.canceled_at = new Date();
+      await appointment.save();
 
       return res.json(appointment);
     } catch (err) {
