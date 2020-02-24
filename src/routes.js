b3a6803739fdf41a5832a1d3c4487d8ef3eb0ed5
arthur-es <arthurcastroes@gmail.com>
2020-02-24 10:54:50 -0300

    route available for given provider id and date

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import ProviderController from "./app/controllers/ProviderController";
 import AppointmentController from "./app/controllers/AppointmentController";
 import ScheduleController from "./app/controllers/ScheduleController";
 import NotificationController from "./app/controllers/NotificationController";
+import AvailableController from "./app/controllers/AvailableController";
 
 import authMiddleware from "./app/middlewares/auth";
 
@@ -18,17 +19,23 @@ const upload = multer(multerConfig);
 routes.post("/users", UserController.store);
 routes.post("/sessions", SessionController.store);
 
+//from this point foward the users need to authenticate
 routes.use(authMiddleware);
 
 routes.get("/providers", ProviderController.index);
+routes.get("/providers/:providerId/available", AvailableController.index);
+
 routes.post("/files", upload.single("file"), FileController.store);
+
 routes.get("/appointments", AppointmentController.index);
 routes.post("/appointments", AppointmentController.store);
 routes.delete("/appointments/:id", AppointmentController.delete);
+
 routes.get("/notifications", NotificationController.index);
 routes.put("/notifications/:id", NotificationController.update);
 
 routes.get("/schedule", ScheduleController.index);
+
 routes.put("/users", UserController.update);
 
 export default routes;
