78ba51f5abe2cba0f9dcda05f668dd052e78169c
arthur-es <arthurcastroes@gmail.com>
2020-02-24 11:49:03 -0300

    creating env variables

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,9 +1,9 @@
 export default {
-  host: "smtp.mailtrap.io",
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   auth: {
-    user: "bc8501dee201c1",
-    pass: "85fd624fd88370"
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
   },
   secure: false,
   default: {
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,4 @@
 export default {
-  dsn: "https://8d471dac09b84559b5a8e1d1f0968fee@sentry.io/2822327"
+  dsn: process.env.SENTRY_DSN
 };
+//"https://8d471dac09b84559b5a8e1d1f0968fee@sentry.io/2822327"
